1d89688cb8ed2397b65e3774720a4c6a021d16fc
Warhammer2000 <rustam.r2005@gmail.com>
2023-04-08 19:16:12 +0500

    Delete

--- OOP Zenject/Assets/Resources/Scripts/Player/PlayerAction.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PlayerAction 
-{
-    public abstract void Execute(GameObject playerObject);
-}
